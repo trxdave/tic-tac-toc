5118a7f1ea78c0ae6b2ad59fd563f56624598fb5
David Cotter <daveguy28@gmail.com>
2024-03-14 13:02:40 +0000

    Setting up readme, main.py and document/flowchart

---
+++ main.py
@@ -0,0 +1,7 @@
+def drawBoard(board):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
