689fdced904f8fa4659aa31aba887bcf2d057976
David Cotter <daveguy28@gmail.com>
2024-03-15 15:41:58 +0000

    description what strings is

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 def drawBoard(board):
+    """ 
+    Board is a list of 10 strings representing the board 
+    """
     print(board[7] + '|' + board[8] + '|' + board[9])
     print('-+-+-')
     print(board[4] + '|' + board[5] + '|' + board[6])
@@ -7,6 +10,9 @@ def drawBoard(board):
     print(board[1] + '|' + board[2] + '|' + board[3])
 
 def playerLetter():
+    """
+    The player pick which letter they want to be and computer's letter as the second.
+    """
     player = ''
     while not (player == 'X' or player == 'O'):
         print("Do you want to be X or O?")
@@ -18,12 +24,13 @@ def playerLetter():
         return ['O', 'X']
 
 def whoGoesFirst():
-    if random.randint(0, 1) == 0:
+    """
+    Choose which player goes first.
+    """
+    if random.pick(0, 1) == 0:
         return 'computer'
     else:
         return 'player'
-
-
 
 def makeMove():
 
