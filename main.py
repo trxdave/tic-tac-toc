9a0e9ab62e50041e6ed2437b1d34402a80b46c63
David Cotter <daveguy28@gmail.com>
2024-03-19 10:02:43 +0000

    Gameover wins and draws

--- main.py
+++ main.py
@@ -21,6 +21,17 @@ def gameover():
     or (board[2] == board[4] == board[6] != " ") or (board[0] == board[3] == board [6] != " ")
     or (board[1] == board[4] == board[7] != " ") or (board[2] == board[5] == board [8] != " ")
 
+if gameover():
+    if x_to_play:
+        print("O wins!")
+        someone_won = True
+    if not(x_to_play):
+        print("X wins!")
+        someone_won = True
+if out_of_moves and not(someone_won):
+    print("Draw!")
+
+
 def playerLetter():
     """
     The player pick which letter they want to be and computer's letter as the second.
@@ -44,6 +55,42 @@ def whoGoesFirst():
     else:
         return 'player'
 
+
+while not (gameover()):
+    if x_to_play:
+        move = int(input("It's X's turn. Chose 1-9"))
+        if move<10 and move>0:
+            if board[move-1] == "X" or board[move-1] == "0":
+                used_tile = True
+                moves_done = moves_done - 1
+            else:
+                invalid_input = True
+                moves_done = moves_done - 1
+            else:
+        move = int(input("It's O's turn. Choose 1-9 "))
+        if move<10 and move>0:
+        if board[move-1] == "X" or board[move-1] == "O":
+          used_tile = True
+          moves_done = moves_done - 1
+            else:
+                board[move-1] = "O"
+            else:
+                invalid_input = True
+        moves_done = moves_done - 1
+        if used_tile == False and invalid_input == False:
+        x_to_play = not(x_to_play)
+        os.system("clear")
+        printboard()
+        elif invalid_input == True:
+        print("Invalid input! Try again.")
+        invalid_input = False
+            else:
+                print("A player already chose this spot! Try again.")
+                used_tile = False
+        moves_done = moves_done + 1
+        if moves_done == 9:
+            out_of_moves = True
+            break
 
 
     
