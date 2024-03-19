7161b769f30085acfe641b648faddb99f68418c7
David Cotter <daveguy28@gmail.com>
2024-03-19 09:08:15 +0000

    gameover who is winner

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def printBoard():
     print('-+-+-')
     print(board[1] + '|' + board[2] + '|' + board[3])
 
+def gameover():
+    """
+    Gameover who is winner
+    """
+    return (board[0] == board[1] == board[2] != " ") or (board[3] == board[4] == board[5] != " ")
+    or (board[6] == board[7] == board[8] != " ") or (board[0] == board[4] == board[8] != " ")
+    or (board[2] == board[4] == board[6] != " ") or (board[0] == board[3] == board [6] != " ")
+    or (board[1] == board[4] == board[7] != " ") or (board[2] == board[5] == board [8] != " ")
 
 def playerLetter():
     """
