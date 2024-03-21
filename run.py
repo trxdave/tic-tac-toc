5dd61a2d2139466b8cbc4bbc5a3091fa9e6338ae
David Cotter <daveguy28@gmail.com>
2024-03-21 11:47:22 +0000

    Readme to be create

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ def printBoard():
     Print the tic-tac-toe board
     """
     print(f'{board[0]} | {board[1]} | {board[2]}')
-    print('-+-+-+-+-')
+    print('---------')
     print(f'{board[3]} | {board[4]} | {board[5]}')
-    print('-+-+-+-+-')
+    print('---------')
     print(f'{board[6]} | {board[7]} | {board[8]}')
 
 def gameover():
